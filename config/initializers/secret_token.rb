1e1382776a0e1e51057001bdacc11329241742e7
speterson86 <speterson86@student.cvtc.edu>
2013-11-13 20:11:32 +0000

    Initialize StudentJobs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentJobs::Application.config.secret_key_base = '62eae657ff93127b90f8f3c0c1f5330facba320249a2e5420de4db68ad178402d75128a53aecb5ba6e06200f7eb27415fb88fe69ee06824cc95479071f0c4888'
