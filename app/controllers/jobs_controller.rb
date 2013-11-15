5f18b59095052bd3796950d9c447a2d342120fcb
Lou Mortenson <robotabot@gmail.com>
2013-11-15 16:36:03 +0000

    Define create method.  Add post_params method to require and permit field attributes.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,6 +6,15 @@ class JobsController < ApplicationController
   end
   
   def create
-    render text: params[:post].inspect
+    #render text: params[:post].inspect
+    @job = Job.new(post_params)
+    @job.save
+    redirect_to @job
   end
+  
+  private 
+    def post_params
+      params.require(:post).permit(:title, :duties, :hourly_pay, :company_name, :skills, :desc, :hours_per_week, :shift, :contact_info)
+    end
+
 end
