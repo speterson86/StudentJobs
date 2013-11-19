266879d2503539bc74cf098c624f6493e6363297
Brett Clark <bclark23@student.cvtc.edu>
2013-11-19 00:32:56 +0000

    Made changes so that all the objects matched

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,44 +1,44 @@
 class JobsController < ApplicationController
   
   def index
-    @job = Job.all
+    @jobs = Job.all
   end
   
   def new
-    @job = Job.new
+    @jobs = Job.new
   end
   
   def create
-    @job = Job.new(params[:job].permit(:title, :text))
+    @jobs = Job.new(params[:job].permit(:title, :text))
     
-    if @post.save
-    redirect_to @job
+    if @jobs.save
+    redirect_to @jobs
     else
       render 'new'
     end
   end
   
   def show
-    @job = Job.find(params[:id])
+    @jobs = Job.find(params[:id])
   end
   
   def edit
-    @job = Job.find(params[:id])
+    @jobs = Job.find(params[:id])
   end
   
   def update
-    @job =Job.find(params[:id])
+    @jobs =Job.find(params[:id])
     
-    if @job.update(params[:job].permit(:title, :text))
-      redirect_to @job
+    if @jobs.update(params[:job].permit(:title, :text))
+      redirect_to @jobs
     else
       render 'edit'
     end
   end
   
   def destroy
-  @job = Post.find(params[:id])
-  @job.destroy
+  @jobs = Job.find(params[:id])
+  @jobs.destroy
  
   redirect_to jobs_path
 end
@@ -48,14 +48,14 @@ end
       params.require(:job).permit(:title, :text)
   end
   
-    @job = Job.new(job_params)
-    @job.save
-    redirect_to @job
+    @jobs = Job.new(job_params)
+    @jobs.save
+    redirect_to @jobs
   end
   
   def show
     #the @job instance var is passed to the view
-    @job = Job.find(params[:id])
+    @jobs = Job.find(params[:id])
   end
   
   private 
