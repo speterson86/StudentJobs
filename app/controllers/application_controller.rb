8250b8b847aceb22f09277f4d1e607b17233a927
speterson86 <speterson86@student.cvtc.edu>
2013-11-27 22:32:35 +0000

    User login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  http_basic_authenticate_with name: "admin", password: "cartman", except: [:index, :new, :show]
-end
+  helper_method :current_user
+  def current_user
+    @current_user
+  end
+
+  
+  private
+  def authenticate_user
+    if user = authenticate_with_http_basic {|user, password| User.authenticate(user, password)}
+      @current_user = user
+    else
+      request_http_basic_authentication
+    end
+  end
+  
+end #end of class
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122212209) do
+ActiveRecord::Schema.define(version: 20131127215045) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20131122212209) do
     t.boolean  "approve"
     t.boolean  "reject"
     t.text     "reject_description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
