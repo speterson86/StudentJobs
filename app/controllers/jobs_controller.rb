01eba8c5fdf7d076a5018560191795594d813dd2
Lou Mortenson <robotabot@gmail.com>
2013-12-05 19:26:43 +0000

    Finish admin-index and edit views. Add Job update test. Finish edit and update actions in job controller.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,16 @@
- class JobsController < ApplicationController
-   before_filter :authenticate_user, except: [:index, :new, :show]#use only on controller that we want to protect.
-   def index #method for index.html.erb
+class JobsController < ApplicationController
+  before_filter :authenticate_user, except: [:index, :new, :show]#use only on controller that we want to protect.
+  def index #method for index.html.erb
     @jobs = Job.all#fetch all jobs from controllers/jobs.rb
     #@jobs = Job.where :approve => true#fetch jobs that the admin approved from controllers/jobs.rb
   end
   
-   def new #method for new.html.erb
+  def new #method for new.html.erb
     @job = Job.new
   end
   
-   def create #event handler that handles the data that the user put in new.html.erb after he/she hits the submit button
-     @job = Job.new(job_params)#job_params = get all data filled into new.html.erb form
+  def create #event handler that handles the data that the user put in new.html.erb after he/she hits the submit button
+    @job = Job.new(job_params)#job_params = get all data filled into new.html.erb form
     
     if @job.save
       redirect_to @job
@@ -30,10 +30,10 @@
   end
   
   def update
-  @job = Job.find(params[:id])
- 
-    if @job.update_attributes(params[:post].permit(:title, :duties, :hourly_pay, :company_name, :skills, :desc, :hours_per_week, :shift, :company_website, :email))
-      redirect_to @job
+    @job = Job.find(params[:id])
+    #if @job.update_attributes(params[:post].permit(:title, :duties, :hourly_pay, :company_name, :skills, :desc, :hours_per_week, :shift, :company_website, :email))
+    if @job.update_attributes(admin_update_params)
+      redirect_to admin_index_url
     else
       render 'edit'
     end
@@ -43,5 +43,8 @@
   def job_params
     params.require(:job).permit(:title, :duties, :hourly_pay, :company_name, :skills, :desc, :hours_per_week, :shift, :company_website, :email)
   end
-
+  
+  def admin_update_params
+    params.require(:job).permit(:title, :duties, :hourly_pay, :company_name, :skills, :desc, :hours_per_week, :shift, :company_website, :email)
+  end
 end
