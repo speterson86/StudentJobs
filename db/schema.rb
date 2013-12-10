7c8cb9394755590583696e5b9e1524c6bc0fa437
Brett Clark <bclark23@student.cvtc.edu>
2013-12-10 16:28:11 +0000

    Add css for approve and reject job colors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203211529) do
+ActiveRecord::Schema.define(version: 20131209015235) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20131203211529) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "company_website"
-    t.boolean  "reject"
     t.text     "reject_description"
     t.boolean  "approve",                                    default: false
+    t.boolean  "reject",                                     default: false
   end
 
   create_table "users", force: true do |t|
